4a19c87f8e2bf206519e5c03d73de2fa761cfce8
Rahma Ali <rahma.ma078@gmail.com>
2024-04-02 23:23:17 +0100

    create the login page

---
+++ client/js/login.js
@@ -0,0 +1,35 @@
+const loginForm = document.getElementById("loginForm");
+const regHere = document.getElementById("regHere");
+
+loginForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const form = new FormData(e.target);
+
+    const options = {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email: form.get("email"),
+            password: form.get("password")
+        })
+    }
+
+    const response = await fetch("https://nexusolve-server.onrender.com/profiles/login", options);
+    const data = await response.json();
+
+    if (response.status == 200) {
+        localStorage.setItem("token", data.token);
+        e.target.reset();
+        window.location.assign("dashboard.html");
+    } else {
+        alert(data.error);
+    }
+})
+
+regHere.addEventListener('click', () => {
+    window.location.assign("register.html");
+})
