4371bbf23c55bbc5c9282380a3541260b214816a
Rahma Ali <rahma.ma078@gmail.com>
2024-04-09 23:31:38 +0100

    add a landing page

---
+++ client/js/index.js
@@ -0,0 +1,10 @@
+const regHere = document.getElementById('regHere');
+const loginHere = document.getElementById('loginHere');
+
+regHere.addEventListener('click', () => {
+    window.location.assign("../html/register.html");
+})
+
+loginHere.addEventListener('click', () => {
+    window.location.assign("../html/login.html");
+})
