5c3b6c9432daf2f66a90ae2af38348df2e7d861e
Rahma Ali <rahma.ma078@gmail.com>
2024-05-20 23:13:14 +0100

    check changes to account page

---
+++ client/js/check.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    const nav = document.getElementById("popup-nav");
+    if (nav.style.display === "block") {
+      nav.style.display = "none";
+    } else {
+      nav.style.display = "block";
+    }
+}
