005444e0e9d26fa9a9a26b58088b127131f804c0
Rahma Ali <rahma.ma078@gmail.com>
2024-05-22 15:37:11 +0100

    update the about page

--- client/js/about.js
+++ client/js/about.js
@@ -3,16 +3,55 @@ if (!token) {
     window.location.href = 'login.html';
 }
 
-const homePage = document.getElementById("navHome")
-const accPage = document.getElementById("navAcc")
-const logout = document.getElementById("navLogout")
+async function handleTokenExpiration(token) {
+    try {
+        const tokenDataRes = await fetch(`https://nexusolve-server.onrender.com/profiles/token/${token}`);
+        const tokenData = await tokenDataRes.json();
+        const currentTime = new Date().getTime();
+
+        if (currentTime > tokenData.expiration_timestamp) {
+            localStorage.clear()
+            window.location.href = 'login.html';
+        }
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
+}
+
+handleTokenExpiration(token);
+
+const homePage = document.getElementById("nav-home")
+const accPage = document.getElementById("nav-account")
+const logout = document.getElementById("nav-logout")
+const homePageMob = document.getElementById("mob-nav-home")
+const accPageMob = document.getElementById("mob-nav-account")
+const logoutMob = document.getElementById("mob-nav-logout")
+
+function toggleMenu() {
+    const nav = document.getElementById("popup-nav");
+    if (nav.style.display === "block") {
+      nav.style.display = "none";
+    } else {
+      nav.style.display = "block";
+    }
+}
 
 homePage.addEventListener('click', function(e) {
     e.preventDefault();
     window.location.href = "dashboard.html"
 });
 
+homePageMob.addEventListener('click', function(e) {
+    e.preventDefault();
+    window.location.href = "dashboard.html"
+});
+
 accPage.addEventListener('click', function(e) {
+    e.preventDefault();
+    window.location.href = "account.html"
+});
+
+accPageMob.addEventListener('click', function(e) {
     e.preventDefault();
     window.location.href = "account.html"
 });
@@ -32,10 +71,41 @@ logout.addEventListener('click', async (e) => {
     const response = await fetch(`https://nexusolve-server.onrender.com/profiles/token/${token}`, options);
 
     if (response.ok) {
-        window.location.href = "login.html"
         localStorage.clear()
+        window.location.href = "login.html"
     } else {
         const data = await response.json();
         alert(data.error);
     }
 });
+
+logoutMob.addEventListener('click', async (e) => {
+
+    const token = localStorage.getItem('token');
+
+    const options = {
+        method: "DELETE",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    }
+
+    const response = await fetch(`https://nexusolve-server.onrender.com/profiles/token/${token}`, options);
+
+    if (response.ok) {
+        localStorage.clear()
+        window.location.href = "login.html"
+    } else {
+        const data = await response.json();
+        alert(data.error);
+    }
+});
+
+window.onload = function() {
+    const sectionId = window.location.hash.substring(1);
+    const section = document.getElementById(sectionId);
+    if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+    }
+};
